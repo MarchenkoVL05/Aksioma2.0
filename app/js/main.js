8eefed802b864523426aabe651a544bc56f57710
HP 6 <Shamoointhedark@gmail.com>
2022-12-22 11:19:07 +0500

    Битрикс24: Услуги + Внутренняя страница услуги

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,41 @@
-import { checkCookies } from "./cookie.js";
 window.addEventListener("DOMContentLoaded", () => {
+  function setCookie(name, value, days) {
+    let expires = "";
+    if (days) {
+      let date = new Date();
+      date.setTime(date.getTime() + days * 24 * 60 * 60 * 1000);
+      expires = "; expires=" + date.toUTCString();
+    }
+    document.cookie = name + "=" + (value || "") + expires + "; path=/";
+  }
+
+  function getCookie(name) {
+    let matches = document.cookie.match(
+      new RegExp("(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, "\\$1") + "=([^;]*)")
+    );
+    return matches ? decodeURIComponent(matches[1]) : undefined;
+  }
+
+  function checkCookies() {
+    let cookieNote = document.querySelector(".cookie-notification");
+    let cookieBtnAccept = cookieNote.querySelector(".cookie-notification__btn");
+
+    // Если куки cookies_policy нет или она просрочена, то показываем уведомление
+    if (!getCookie("cookies_policy") && cookieNote) {
+      cookieNote.style.display = "flex";
+    }
+
+    // При клике на кнопку устанавливаем куку cookies_policy на один год
+    if (cookieBtnAccept) {
+      cookieBtnAccept.addEventListener("click", function () {
+        setCookie("cookies_policy", "true", 1);
+        cookieNote.style.display = "none";
+      });
+    }
+  }
+
   checkCookies();
+
   // Анимация появления слева
   let fadeInLeft = document.querySelectorAll(".fadeInLeft");
   let fadeInTop = document.querySelectorAll(".fadeInTop");
